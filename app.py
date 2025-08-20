ccb8ebe493ac7f0a4adbd4da4a91be93ce5644d9
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-08-20 21:16:37 +0530

    google login validation test v1

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
+
+if not st.user.is_logged_in:
+    if st.button("Log in with Google"):
+        st.login()
+    st.stop()
+
+if st.button("Log out"):
+    st.logout()
+st.markdown(f"Welcome! {st.user.name}")
+
+
 st.write("Welcome to FETQuest OneView")
 st.write("App is on construction")
